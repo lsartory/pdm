c0d0366a6ab27729123b1e140a79b956edf13515
Lambert Sartory <lambert.sartory@gmail.com>
2025-04-13 21:06:04 +0200

    Code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -11,157 +11,98 @@
 
 /******************************************************************************/
 
-/// A trait that allow generic modulator implementations
-pub trait Modulator {
-    /// The data type used for the integrator
-    type SigmaType;
-}
-
-/******************************************************************************/
-
 /// Pulse Density Modulator
-///
-/// Contains the current setpoint and integrator values
 pub struct Pdm<T: Modulator> {
     value: T,
     sigma: [T::SigmaType; 2]
 }
 
-impl<T: Modulator> Pdm<T> {
+impl<T> Default for Pdm<T>
+where
+    T: Modulator,
+    T::SigmaType: Modulator
+{
+    fn default() -> Self {
+        assert!(T::DELTA_POS != T::DELTA_NEG, "Unsupported type!");
+        Self {
+            value: T::ZERO,
+            sigma: [T::SigmaType::ZERO; 2]
+        }
+    }
+}
+
+impl<T> Pdm<T>
+where
+    T: Modulator,
+    T::SigmaType: Modulator
+{
+    /// Initializes a new modulator
+    pub fn new() -> Self {
+        Default::default()
+    }
+
     /// Sets the modulator value
+    ///
+    /// Valid range is 0..T::MAX for unsigned types, -T::MAX..T::MAX for signed types, and -1.0..1.0 for
+    /// float types.
     pub fn set_value(&mut self, value: T) {
         self.value = value
+    }
+
+    /// Returns the next output value of the modulator
+    pub fn update(&mut self) -> bool {
+        let delta = if self.sigma[1] >= T::SigmaType::ZERO {
+            T::DELTA_NEG
+        } else {
+            T::DELTA_POS
+        };
+        self.sigma[0] = self.sigma[0] + self.value.into() + delta;
+        self.sigma[1] = self.sigma[1] + self.sigma[0] + delta;
+        self.sigma[1] >= T::SigmaType::ZERO
     }
 }
 
 /******************************************************************************/
 
-macro_rules! gen_unsigned_impl {
-    ($T: ty, $S: ty) => {
+/// A trait that allow generic modulator implementations
+pub trait Modulator: Copy + core::ops::Add<Output = Self> + Into<Self::SigmaType> {
+    /// The data type used for the integrator
+    type SigmaType: PartialEq + PartialOrd;
+
+    /// Zero constant
+    const ZERO: Self;
+    /// Positive delta value
+    const DELTA_POS: Self::SigmaType;
+    /// Negative delta value
+    const DELTA_NEG: Self::SigmaType;
+}
+
+macro_rules! modulator_impl {
+    ($T: ty, $S: ty, $ZERO: literal, $DELTA_POS: expr, $DELTA_NEG: expr) => {
         impl Modulator for $T {
             type SigmaType = $S;
-        }
 
-        impl Pdm<$T> {
-            /// Initializes a new modulator
-            pub fn new() -> Self {
-                Self {
-                    value: 0,
-                    sigma: [0; 2]
-                }
-            }
-
-            /// Returns the next output value of the modulator
-            pub fn update(&mut self) -> bool {
-                let mut sigma_new: [$S; 2] = [0; 2];
-                if self.sigma[1] >= 0 {
-                    sigma_new[0] = self.sigma[0] + self.value as $S - <$T>::MAX as $S;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0] - <$T>::MAX as $S;
-                } else {
-                    sigma_new[0] = self.sigma[0] + self.value as $S;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0];
-                }
-                self.sigma = sigma_new;
-                self.sigma[1] >= 0
-            }
+            const ZERO: Self = $ZERO;
+            const DELTA_POS: Self::SigmaType = $DELTA_POS;
+            const DELTA_NEG: Self::SigmaType = $DELTA_NEG;
         }
     };
 }
 
-gen_unsigned_impl!(u64, i128);
-gen_unsigned_impl!(u32, i64);
-gen_unsigned_impl!(u16, i32);
-gen_unsigned_impl!(u8, i16);
+modulator_impl!(i128, i128, 0, 0, 0);
 
-/******************************************************************************/
+modulator_impl!(i64, i128, 0, i64::MAX as i128, -(i64::MAX as i128));
+modulator_impl!(i32, i64,  0, i32::MAX as i64,  -(i32::MAX as i64));
+modulator_impl!(i16, i32,  0, i16::MAX as i32,  -(i16::MAX as i32));
+modulator_impl!(i8,  i16,  0,  i8::MAX as i16,  -( i8::MAX as i16));
 
-macro_rules! gen_signed_impl {
-    ($T: ty, $S: ty) => {
-        impl Modulator for $T {
-            type SigmaType = $S;
-        }
+modulator_impl!(u64, i128, 0, 0, -(u64::MAX as i128));
+modulator_impl!(u32, i64,  0, 0, -(u32::MAX as i64));
+modulator_impl!(u16, i32,  0, 0, -(u16::MAX as i32));
+modulator_impl!(u8,  i16,  0, 0, -( u8::MAX as i16));
 
-        impl Default for Pdm<$T> {
-            fn default() -> Self {
-                Self {
-                    value: 0,
-                    sigma: [0; 2]
-                }
-            }
-        }
-
-        impl Pdm<$T> {
-            /// Initializes a new modulator
-            pub fn new() -> Self {
-                Default::default()
-            }
-
-            /// Returns the next output value of the modulator
-            pub fn update(&mut self) -> bool {
-                let mut sigma_new: [$S; 2] = [0; 2];
-                if self.sigma[1] >= 0 {
-                    sigma_new[0] = self.sigma[0] + self.value as $S - <$T>::MAX as $S;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0] - <$T>::MAX as $S;
-                } else {
-                    sigma_new[0] = self.sigma[0] + self.value as $S + <$T>::MAX as $S;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0] + <$T>::MAX as $S;
-                }
-                self.sigma = sigma_new;
-                self.sigma[1] >= 0
-            }
-        }
-    };
-}
-
-gen_signed_impl!(i64, i128);
-gen_signed_impl!(i32, i64);
-gen_signed_impl!(i16, i32);
-gen_signed_impl!(i8, i16);
-
-/******************************************************************************/
-
-macro_rules! gen_float_impl {
-    ($T: ty) => {
-        impl Modulator for $T {
-            type SigmaType = $T;
-        }
-
-        impl Default for Pdm<$T> {
-            fn default() -> Self {
-                Self {
-                    value: 0.0,
-                    sigma: [0.0; 2]
-                }
-            }
-        }
-
-        impl Pdm<$T> {
-            /// Initializes a new modulator
-            ///
-            /// Values are expected between -1.0 and 1.0
-            pub fn new() -> Self {
-                Default::default()
-            }
-
-            /// Returns the next output value of the modulator
-            pub fn update(&mut self) -> bool {
-                let mut sigma_new: [$T; 2] = [0.0; 2];
-                if self.sigma[1] >= 0.0 {
-                    sigma_new[0] = self.sigma[0] + self.value as $T - 1.0 as $T;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0] - 1.0 as $T;
-                } else {
-                    sigma_new[0] = self.sigma[0] + self.value as $T + 1.0 as $T;
-                    sigma_new[1] = self.sigma[1] + sigma_new[0] + 1.0 as $T;
-                }
-                self.sigma = sigma_new;
-                self.sigma[1] >= 0.0
-            }
-        }
-    };
-}
-
-gen_float_impl!(f64);
-gen_float_impl!(f32);
+modulator_impl!(f32, f32, 0.0, 1.0, -1.0);
+modulator_impl!(f64, f64, 0.0, 1.0, -1.0);
 
 /******************************************************************************/
 
@@ -199,15 +140,15 @@ mod tests {
                 let mut pdm = Pdm::<$T>::new();
                 pdm.set_value($setpoint);
 
-                let mut avg = 0;
+                let mut avg = 0.0;
                 for _ in 0..$iterations {
-                    if pdm.update() {
-                        avg += <$T>::MAX as i128;
+                    avg += if pdm.update() {
+                        <$T>::DELTA_POS
                     } else {
-                        avg -= <$T>::MAX as i128;
-                    }
+                        <$T>::DELTA_NEG
+                    } as f64;
                 }
-                let ratio = (avg as f64 / $iterations as f64) / $setpoint as f64;
+                let ratio = (avg / $iterations as f64) / $setpoint as f64;
                 assert!(ratio >= 0.99 && ratio <= 1.01, "ratio: {}", ratio);
             }
         };
@@ -217,26 +158,6 @@ mod tests {
     gen_signed_test!(test_i32, i32, -420_000_000, 2_000_000);
     gen_signed_test!(test_i64, i64, 1.223e18 as i64, 2_000_000);
 
-    macro_rules! gen_float_test {
-        ($name: ident, $T: ty, $setpoint: expr, $iterations: literal) => {
-            #[test]
-            fn $name() {
-                let mut pdm = Pdm::<$T>::new();
-                pdm.set_value($setpoint);
-
-                let mut avg = 0.0;
-                for _ in 0..$iterations {
-                    if pdm.update() {
-                        avg += 1.0;
-                    } else {
-                        avg -= 1.0;
-                    }
-                }
-                let ratio = (avg / $iterations as f64) / $setpoint as f64;
-                assert!(ratio >= 0.99 && ratio <= 1.01, "ratio: {}", ratio);
-            }
-        };
-    }
-    gen_float_test!(test_f32, f32, 0.42, 500_000);
-    gen_float_test!(test_f64, f64, -0.42, 500_000);
+    gen_signed_test!(test_f32, f32, 0.42, 500_000);
+    gen_signed_test!(test_f64, f64, -0.42, 500_000);
 }
